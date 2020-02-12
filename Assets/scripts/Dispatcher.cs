64ac4beb90171c34183b44c5ad9da391ac561a2e
PioneerOld <60870458+PioneerOld@users.noreply.github.com>
2020-02-12 10:39:20 +0500

    Add files via upload

--- Assets/scripts/Dispatcher.cs
+++ Assets/scripts/Dispatcher.cs
@@ -6,56 +6,65 @@ using UnityEngine.UI;
 public class Dispatcher : MonoBehaviour
 {
     static Dictionary<string, int> shipsLeftToAllocate = new Dictionary<string, int>();
-    static Dictionary<string, Text> labelF = new Dictionary<string, Text>();
+    static Dictionary<string, Text> shipsLabels = new Dictionary<string, Text>();
 
+    bool isWorkingInstance = true;
     public GameObject shipPrefab;
     protected static Ship currentShip;
     string dictKey;
     string floorNum;
     // Start is called before the first frame update
     protected virtual void Start()
+
     {
+        isWorkingInstance = name.Contains("(Clone)");
         dictKey = gameObject.name.Replace("(Clone)", null);
         floorNum = dictKey.Replace("Ship", null);
         var shipsToAllocate = 5 - int.Parse(floorNum);
-        if (!shipsLeftToAllocate.ContainsKey(dictKey)) shipsLeftToAllocate.Add(dictKey, shipsToAllocate);
+        if (!shipsLeftToAllocate.ContainsKey(dictKey))
+        {
+            shipsLeftToAllocate.Add(dictKey, shipsToAllocate);
+            FillLabels();
+        } 
         RefreshStrings();
-        FillLabels();
     }
     protected void OnShipClick()
     {
-        if (gameObject.name.Contains("(Clone)")) // Проверям кораблик игровой или витринный
+        if (isWorkingInstance) // Проверям кораблик игровой или витринный
         {
-            if (currentShip == null) currentShip = GetComponentInChildren<Ship>();
-            else if (currentShip.WasAllocatedOnce()) currentShip = null;
+            if (currentShip == null)
+            {
+                currentShip = GetComponentInChildren<Ship>();
+            }
+            else if (currentShip.IsPositionCorrect)
+            {
+                if (!currentShip.WasAllocatedOnce()) shipsLeftToAllocate[dictKey]--;
+                Debug.Log(shipsLeftToAllocate[dictKey]);
+                RefreshStrings();
+                currentShip = null;
+            }
         }
         else if (currentShip == null) // sample template
+        
         {
             if (shipsLeftToAllocate[dictKey] < 1) return;
             var shipToPlay = Instantiate(shipPrefab, transform.parent.transform);
             currentShip = shipToPlay.GetComponentInChildren<Ship>();
-            shipsLeftToAllocate[dictKey]--;
-            RefreshStrings();
         }
     }
     void OnDestroy()
     {
-        shipsLeftToAllocate[dictKey]++;
-        RefreshStrings();
+       
     }
 
     void FillLabels()
     {
-        var objectLabel = GameObject.Find(dictKey+"label");
-        Debug.Log(dictKey+"label");
-
+        var textBlock = GameObject.Find("label "+dictKey).GetComponent<Text>();
+        shipsLabels.Add(textBlock.name.Replace("label ", null), textBlock);
     }
 
     protected void RefreshStrings()
     {
-        var labels = transform.parent.GetComponentsInChildren<Text>();
-        foreach (var label in labels)
-            if (label.gameObject.name.Contains("label" + floorNum))
-                label.text = shipsLeftToAllocate[dictKey].ToString();
+        shipsLabels[dictKey].text = shipsLeftToAllocate[dictKey] + "x";
     }
 }
